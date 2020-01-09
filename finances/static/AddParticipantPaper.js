054b6637f587ad5c0cdeb802ffb5f97437458b16
zlira <olesia.hr@gmail.com>
2020-01-09 20:38:45 +0200

    Split price to base and donation in add participant paper

---
+++ finances/static/AddParticipantPaper.js
@@ -0,0 +1,95 @@
+let FIELDSETS = []
+
+
+async function fetchPaperData(paper_id) {
+    const url = new URL(
+      '/finances/paper/',
+      window.location.protocol + '//' + window.location.host
+    )
+    url.search = new URLSearchParams({paper_id: paper_id})
+    const res = await fetch(url)
+    return await res.json()
+}
+
+
+class Fieldset {
+    constructor(node) {
+        this.node = node
+        this.paperField = this.node.querySelector('.field-paper select')
+        this.priceField = this.node.querySelector('.field-price input')
+        this.currentPaperPrice = undefined
+        this.totalPrice = this.priceField.value
+        this.priceLabel = undefined
+
+        this.getCurrentPaperId = this.getCurrentPaperId.bind(this)
+        this.initEventListeners = this.initEventListeners.bind(this)
+        this.onPaperChange = this.onPaperChange.bind(this)
+        this.onPriceChange = this.onPriceChange.bind(this)
+        this.updatePriceLabel = this.updatePriceLabel.bind(this)
+
+        this.initEventListeners()
+    }
+
+    initEventListeners() {
+        this.paperField.addEventListener('change', this.onPaperChange)
+        this.priceField.addEventListener('change', this.onPriceChange)
+    }
+
+    getCurrentPaperId() {
+        return this.paperField.value
+    }
+
+    onPaperChange() {
+        let currentPaperId = this.getCurrentPaperId()
+        if (!currentPaperId) {
+            this.currentPaperPrice = undefined
+            this.updatePriceLabel()
+            return
+        }
+        fetchPaperData(currentPaperId).then(
+            res => {
+                const paperData = res
+                this.currentPaperPrice = paperData.price
+                this.priceField.value = this.currentPaperPrice
+                this.totalPrice = this.currentPaperPrice
+                this.updatePriceLabel()
+            }
+        )
+    }
+
+    updatePriceLabel() {
+        if (!this.priceLabel) {
+            this.priceLabel = document.createElement('span')
+            this.priceLabel.innerHTML = 'i am ready'
+            this.priceField.parentNode.appendChild(this.priceLabel)
+        }
+        let labelInnerHtml = ''
+        if (this.currentPaperPrice) {
+            const priceDiff = this.totalPrice - this.currentPaperPrice
+            labelInnerHtml =`${this.currentPaperPrice} грн + ${priceDiff} донації`
+        }
+        this.priceLabel.innerHTML = labelInnerHtml
+    }
+
+    onPriceChange() {
+        this.totalPrice = parseInt(this.priceField.value)
+        // debugger
+        if (this.currentPaperPrice && this.currentPaperPrice > this.totalPrice) {
+            this.totalPrice = this.currentPaperPrice
+            this.priceField.value = this.totalPrice
+        }
+        this.updatePriceLabel()
+    }
+}
+
+function initAddPaperFieldsets() {
+    const fieldsetNodes = document.querySelectorAll('.add-participant-paper')
+    // debugger
+    fieldsetNodes.forEach(
+        node => FIELDSETS.push(new Fieldset(node))
+    )
+}
+
+
+// TODO repeat if new paper was added
+document.addEventListener('DOMContentLoaded', initAddPaperFieldsets)
