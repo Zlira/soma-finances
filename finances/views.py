ff1978fc17e35b660a316429b593b91d3a681188
dfkthsq <dfkthsq@gmail.com>
2020-02-06 17:52:30 +0200

    JSONResponse changed

--- finances/views.py
+++ finances/views.py
@@ -1,29 +1,43 @@
-from django.db.models import Value, CharField
-from django.db.models.functions import Concat
+from django.db.models import(
+    Value, CharField, F, DurationField,
+    ExpressionWrapper, fields, Count
+)
+from django.db.models.functions import Cast
 from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
 from django.shortcuts import get_object_or_404
 
 from .models import ParticipantPaper, Paper
-
+from datetime import date
 
 # TODO add authentication
+
 
 def participant_papers(request):
     participant_id = request.GET.get('participant_id')
     if not participant_id:
         return HttpResponseBadRequest('participant_id is required')
     # TODO check if participant exists
+    days_in_use = ExpressionWrapper(
+        Cast(date.today(), fields.DateField()) - F('date_purchased'),
+        output_field=fields.DurationField()
+    )
     res = (ParticipantPaper.objects
            # TODO add expired filter
+           .annotate(
+               days_in_use=days_in_use,
+               times_used=Count('classparticipation')
+           )
            .filter(participant=participant_id)
            .select_related('participant', 'paper')
            .values(
                'id',
-               name=Concat(
-                   'paper__name', Value(' належить '), 'participant__name',
-               )
+               name=F('paper__name'),
+               days_in_use=F('days_in_use'),
+               times_used=F('times_used')
            )
            )
+    for i in res:
+        i['days_in_use'] = i['days_in_use'].days
     return JsonResponse({'participantPapers': list(res)})
 
 
